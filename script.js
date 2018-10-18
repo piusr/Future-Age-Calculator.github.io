82a3ba9ab93022240ee5e691e97f5eba8f30c78a
piusr <40551856+piusr@users.noreply.github.com>
2018-10-18 21:45:53 +0100

    Update
    1. Added more logic to script.js
    2. Added some styles
    3. Added a background image.

--- script.js
+++ script.js
@@ -12,25 +12,35 @@ A simple script that tells you how old you will be at a future specific year
 
 function futureAge() {
 
-	var future = document.getElementById('future-year').value; //Get the value of the year selected in the future
-	var birthYear = document.getElementById('yearOfBirth').value; //Get the value of the future year.
-	
-	var yourAge = parseInt(future - birthYear); //Calculate the difference between both years to get future age.
+	let future = document.getElementById('future-year').value; //Get the value of the year selected in the future
+	let birthYear = document.getElementById('yearOfBirth').value; //Get the value of the future year.	
+	let yourAge = parseInt(future - birthYear); //Calculate the difference between both years to get future age.
+
+	let year = new Date();
+	let currentYear = year.getFullYear();
+
+	console.log(currentYear);
 
 	if (future === ""  && birthYear === "") {
 		return document.getElementById('results').innerHTML = "Hey, You have not selected your Birth year and a Future year.";
-		console.log('You have not chosen a date');
+
 	} else if (birthYear === ""){
 		return document.getElementById('results').innerHTML = "Hey, You have not selected your Birth year.";
 
 	} else if (future === "") {
 		return document.getElementById('results').innerHTML = "Hey, You have not selected a Future year.";
 
+	} else if (future < birthYear) {
+		return document.getElementById('results').innerHTML = "Hey, this calculation is not possible. Your birth year cannot be greater than future year.";
+
+	} else if (future < currentYear) {
+	    return document.getElementById('results').innerHTML = "Hey, in the year <b>"+future+"</b>" +" you were "+"<b>"+ yourAge+"</b>" +"years!";
+               
 	} else {
 		//return the age and pass it into the DOM. 
 
 	return document.getElementById('results').innerHTML = "Hey, by <b>"+future+"</b>" +" you will be "+"<b>"+ yourAge+"</b>" +"years Congratulations!";
-
+               
 	}
 
 	
